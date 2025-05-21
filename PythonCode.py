c4bdf9de4cc3ab7fb806ac31b60950a469411b07
Besler84 <steffen.besler@schule-zukunftsmotor.org>
2025-05-21 21:02:36 +0200

    Erster Python Code

---
+++ PythonCode.py
@@ -0,0 +1,7 @@
+print ("Zukunftsmotor K17 - Erster Python Code")
+
+orte = ["Berlin","Hamburg","Hamm"]
+stadt = "Hamm"
+
+if stadt in orte:
+    print("Die Stadt", stadt, "ist in der Liste vorhanden.")
