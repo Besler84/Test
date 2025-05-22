3e08b0953f7f8260186f3cb8068e5b79bf3d8135
Besler84 <steffen.besler@schule-zukunftsmotor.org>
2025-05-22 16:51:01 +0200

    Aufgabe 314 komplett

--- Aufgabe314/Strings_in_Python.py
+++ Aufgabe314/Strings_in_Python.py
@@ -1,3 +1,17 @@
 print("Strings in Python")
 print("")
+print("String 1")
+print("")
 print('Die Programmiersprache "Python" ist cool !')
+print("")
+print("String 2")
+print("")
+print('''Die 
+Programmiersprache 
+"Python" 
+ist 
+cool !''')
+print("")
+print("String 3")
+print("")
+print('Die Programmiersprache ' + '"Python" ' + 'ist cool !')
---
+++ Aufgabe314/Variablen_in_Python.py
@@ -0,0 +1,9 @@
+age01 = 41
+name01 = 'Steffen Besler'
+print('Variablen in Python')
+print('')
+print('Name:')
+print(name01)
+print('')
+print('Alter:')
+print(age01)
