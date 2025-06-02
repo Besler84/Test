7930265d16b90709f63ed06f8e465949e0759a32
Besler84 <steffen.besler@schule-zukunftsmotor.org>
2025-06-02 11:57:53 +0200

    Aufgabe 332 Final

---
+++ Aufgabe332/TKinter_alias.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+label1 = tk.Label(root, text="Git in TkInter")
+label1.pack()
+
+root.mainloop()
---
+++ Aufgabe332/Tkinter_pack.py
@@ -0,0 +1,8 @@
+import tkinter
+
+root = tkinter.Tk()
+
+label1 = tkinter.Label(root, text="Git in TkInter")
+label1.pack()
+
+root.mainloop()
